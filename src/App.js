eb64df7dd93409946daffa2a02accb84bbff1eb2
maro525 <hdmrf.fjnm@gmail.com>
2020-08-20 12:46:51 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './Game';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p> */}
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        {/* </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <Game />
       </header>
     </div>
   );
